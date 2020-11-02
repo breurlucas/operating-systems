7e104e9474fe3e68e52421c9bf77755ea3382f73
breurlucas <lucasbreur@gmail.com>
2020-11-02 14:03:34 -0300

    Implemented threading per file

--- ex-class09-1/main.c
+++ ex-class09-1/main.c
@@ -4,35 +4,42 @@
 #include <unistd.h>
 #include <pthread.h>
 
-// Compile using std=c17
+void *count_words(void *); // Function prototype
 
 int main(int argc, char *argv[]) {
 
-    FILE *file;
-    int words;
+    int n = argc - 1; // The number of files passed through as arguments
+    pthread_t t[n];     // Thread vector sized 'n'
 
-    // for(int i=1; i<argc; i++) {
-    //     file = fopen(argv[i], "r")
-    // }
-
-    file = fopen(argv[1], "r");
-
-    if(file == NULL) {
-        printf("The requested file does not exist");
-        return 1;
+    // Loop through the 'n' files passed as parameters and create a separate thread for each one
+    for(int i=0; i<n; i++) {
+        pthread_create(&t[i], NULL, count_words, argv[i+1]);
     }
 
-    words = 0;
-
-    words = count_words(file, words);
-
-    printf("%d\n", words); 
-
+    // Wait for the 'n' threads to finish
+    for(int i=0; i<n; i++) {
+        pthread_join(t[i], NULL);
+    }
+    
+    exit(EXIT_SUCCESS);
 }
 
-int count_words(FILE *file, int words) {
+// Wordcount function to be executed by the threads
+void *count_words(void *arg) {
 
-    char head; // Tracks reading head position
+    FILE *file; // File pointer    
+    char head; // Tracks reading head position (STACK, unique for each thread)
+    int wordcount = 0; // Initializes the wordcount variable (STACK, unique for each thread)
+
+    char *filename = (char *) arg; // Cast (void *) to (char *)
+
+    file = fopen(filename, "r"); // Open the file in read-only mode
+
+    // Check if a valid file was found
+    if(file == NULL) {
+        printf("The requested file does not exist or could not be found\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Checks if the file has at least one character/word. If the first character is 'EOF', the wordcount will be zero.
     head = getc(file);
@@ -40,10 +47,12 @@ int count_words(FILE *file, int words) {
     // Read each character one by one till the end of the file is reached
     while(head != EOF) {
         head = getc(file);
-        if(head == ' ' || head == '\n' || head == EOF) { words++; }
+        if(head == ' ' || head == '\n' || head == EOF) { wordcount++; }
     }
 
-    // Closes the file after reading
-    fclose(file);  
-    return words;
+    fclose(file); // Closes the file after reading
+
+    printf("%s: %d words\n", filename, wordcount); // Print the result
+
+    return NULL;
 }
